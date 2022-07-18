ed56bc5c379f74c267ca5b4d709a2b9e5ead9f4a
kilarugowthami <109350487+kilarugowthami@users.noreply.github.com>
2022-07-18 19:45:13 +0530

    initial Project

---
+++ RC_Training/App.js
@@ -0,0 +1,312 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View
+      style={{
+        textAlign: 'center',
+        backgroundColor: 'gray',
+        width: '100%',
+        height: '100%',
+        flex: 1,
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '90%',
+          height: '10%',
+          textAlign: 'center',
+          backgroundColor: 'skyblue',
+          top: 10,
+          margin: 20,
+          borderRadius: 30,
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            width: '10%',
+            height: '50%',
+            top: 15,
+
+            backgroundColor: 'pink',
+            textAlign: 'center',
+
+            marginLeft: 30,
+          }}></View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            width: '10%',
+            height: '50%',
+            top: 15,
+
+            backgroundColor: 'pink',
+            textAlign: 'center',
+
+            marginLeft: 50,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'row',
+            width: '10%',
+            height: '50%',
+            top: 15,
+
+            backgroundColor: 'pink',
+            textAlign: 'center',
+
+            marginLeft: 50,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'row',
+            width: '10%',
+            height: '50%',
+            top: 15,
+
+            backgroundColor: 'pink',
+            textAlign: 'center',
+
+            marginLeft: 30,
+          }}></View>
+      </View>
+      <View
+        style={{
+          // flexDirection: 'row',
+          width: '90%',
+          height: '30%',
+          top: 15,
+          margin: 20,
+
+          backgroundColor: 'red',
+          textAlign: 'center',
+        }}>
+        <View
+          style={{
+            flexDirection: 'column',
+            width: '90%',
+            height: '10%',
+            top: 15,
+            margin: 10,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+
+            marginLeft: 15,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'column',
+            width: '90%',
+            height: '10%',
+            top: 15,
+            margin: 10,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+
+            marginLeft: 15,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'column',
+            width: '90%',
+            height: '10%',
+            top: 15,
+            margin: 10,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+
+            marginLeft: 15,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'column',
+            width: '90%',
+            height: '10%',
+            top: 15,
+            margin: 10,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+
+            marginLeft: 15,
+          }}></View>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '90%',
+          height: '20%',
+          top: 15,
+          margin: 20,
+
+          backgroundColor: 'green',
+          textAlign: 'center',
+        }}>
+        <View
+          style={{
+            flexDirection: 'column',
+            width: '10%',
+            height: '60%',
+            top: 15,
+            margin: 10,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+
+            marginLeft: 15,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'column',
+            width: '10%',
+            height: '60%',
+            top: 15,
+            margin: 10,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+
+            marginLeft: 15,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'column',
+            width: '10%',
+            height: '60%',
+            top: 15,
+            margin: 10,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+
+            marginLeft: 15,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'column',
+            width: '10%',
+            height: '60%',
+            top: 15,
+            margin: 10,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+
+            marginLeft: 15,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'column',
+            width: '10%',
+            height: '60%',
+            top: 15,
+            margin: 10,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+
+            marginLeft: 15,
+          }}></View>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '90%',
+          height: '13%',
+          top: 15,
+          margin: 20,
+
+          backgroundColor: 'yellow',
+          textAlign: 'center',
+        }}>
+        <View
+          style={{
+            flexDirection: 'column',
+            width: '30%',
+            height: '70%',
+            top: 5,
+            margin: 10,
+
+            backgroundColor: 'blue',
+            textAlign: 'center',
+
+            marginLeft: 25,
+          }}>
+          <View
+          style={{
+            flexDirection: 'row',
+            width: '40%',
+            height: '20%',
+            top: 15,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+
+            marginLeft: 20,
+          }}></View>
+          <View
+          style={{
+            flexDirection: 'row',
+            width: '40%',
+            height: '20%',
+            top: 25,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+
+            marginLeft: 20,
+          }}></View>
+          </View>
+          <View
+          style={{
+            flexDirection: 'column',
+            width: '30%',
+            height: '70%',
+            top: 5,
+            margin: 10,
+            borderRadius:20,
+
+            backgroundColor: 'blue',
+            textAlign: 'center',
+
+            marginLeft: 55,
+          }}>
+          <View
+          style={{
+            flexDirection: 'row',
+            width: '20%',
+            height: '20%',
+            top: 25,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+
+            marginLeft: 20,
+            borderRadius:40
+          }}></View>
+          <View
+          style={{
+            flexDirection: 'row',
+            width: '20%',
+            height: '20%',
+            top: 15,
+
+            backgroundColor: 'white',
+            textAlign: 'center',
+            borderRadius:40,
+
+            marginLeft: 55,
+          }}></View>
+          </View>
+
+
+        </View>
+
+    </View>
+  );
+}
